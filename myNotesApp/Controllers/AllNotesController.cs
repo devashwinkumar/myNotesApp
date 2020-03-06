5dc46fb9bf943970970840f1af493a73f86f0e20
ashwin2sr <ashwin2sr@gmail.com>
2020-03-07 00:51:53 +0800

    Some Minor UI Changes

---
+++ myNotesApp/Controllers/AllNotesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myNotesApp.Models;
+
+namespace myNotesApp.Controllers
+{
+    public class AllNotesController : Controller
+    {
+        private myNotesAppDBEntities _context;
+
+        public AllNotesController()
+        {
+            _context = new myNotesAppDBEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: AllNotes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
